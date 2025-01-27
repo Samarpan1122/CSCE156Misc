93fef81f1a7cf8bd0971cd2b3578b3da59692e46
smohanty13@unl.edu <smohanty13@unl.edu@10.65.123.133>
2025-01-27 09:44:16 -0600

    initial commit

---
+++ src/unl/soc/HelloWorld.java
@@ -0,0 +1,17 @@
+package unl.soc;
+
+/**
+ * Samarpan Mohanty 
+ * 2025-01-22
+ * 
+ * Prints 'Hello World' to standard output
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
